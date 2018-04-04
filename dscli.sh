c74141f7e873063e930cb5cbe283bf0082816b49
Kamil Madac <kamil.madac@gmail.com>
2018-04-04 22:12:36 +0200

    using settings.sh is dscli container

--- dscli.sh
+++ dscli.sh
@@ -2,24 +2,9 @@
 
 CONF_FILE=/srv/dietstack/settings.sh
 
-if [ -f $CONF_FILE ]; then
-    echo "Loading settings file $CONF_FILE"
-    . $CONF_FILE
-else
-    echo "ERROR: settings file $CONF_FILE not found!"
-    exit 1
-fi
-
-if [ -z $EXTERNAL_IP ]; then
-    echo "ERROR: EXTERNAL_IP variable must be set!"
-    exit 2
-fi
-
-JUST_EXTERNAL_IP=$(echo $EXTERNAL_IP | cut -d"/" -f 1)
-
 docker run --rm --net=host \
            -v ~/.ssh:/root/.ssh \
-            -v /srv/dietstack/glance-images-osadmin:/app/glance-images \
-           -e JUST_EXTERNAL_IP=$JUST_EXTERNAL_IP \
+           -v /srv/dietstack/glance-images-osadmin:/app/glance-images \
+           -v $CONF_FILE:/app/settings.sh \
            -it dietstack/osadmin /bin/bash
 
